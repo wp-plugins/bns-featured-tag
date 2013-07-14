78691737cd9df36439a47b9796295438fa472e0c
cais <cais@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-14 22:41:26 +0000

    * Add exclude current post in single view option * Completed use current post tags in single view option / functionality
    git-svn-id: https://plugins.svn.wordpress.org/bns-featured-tag/tags/2.4@740695 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bnsft-options.js
@@ -0,0 +1,38 @@
+/**
+ * This file is part of BNS Featured Tag
+ * Last revised at version 2.2
+ *
+ * Copyright 2009-2013  Edward Caissie  (email : edward.caissie@gmail.com)
+ *
+ * BNS Featured Category is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * You may NOT assume that you can use any other version of the GPL.
+ *
+ * BNS Featured Category is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ *
+ *      Free Software Foundation, Inc.
+ *      51 Franklin St, Fifth Floor
+ *      Boston, MA  02110-1301  USA
+ */
+jQuery(document).ready(function($) {
+    // Note: $() will work as an alias for jQuery() inside of this function
+    $("p.bnsft-display-all-posts-check input.checkbox").click(function () {
+        $(".bnsft-all-options-open").toggleClass("bnsft-all-options-closed");
+    });
+
+    $("p.bnsft-display-thumbnail-sizes input.checkbox").click( function(){
+        $(".bnsft-thumbnails-open").toggleClass("bnsft-thumbnails-closed");
+    });
+
+    $("p.bnsft-excerpt-option-open-check input.checkbox").click( function(){
+        $(".bnsft-excerpt-option-open").toggleClass("bnsft-excerpt-option-closed");
+    });
+});
